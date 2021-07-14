c771dd44900004983d2d546c22611df3497e8c27
afdhalkh <afdhal.kh@gmail.com>
2021-07-14 15:58:33 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -200
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile1`, function (sprite, location) {
+    game.over(false, effects.dissolve)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(6)
+tiles.setTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 3 3 3 3 3 3 3 3 . . . . 
+    . . . 3 d 3 3 3 3 3 3 c 3 . . . 
+    . . 3 c d 3 3 3 3 3 3 c c 3 . . 
+    . 3 c c d d d d d d 3 c c d 3 d 
+    . 3 c 3 a a a a a a a b c d 3 3 
+    . 3 3 a b b a b b b a a b d 3 3 
+    . 3 a b b b a b b b b a 3 3 3 3 
+    . a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+    . a a a a a a f a a a f a 3 d d 
+    . a a a a a a f a a f a a a 3 d 
+    . a a a a a a f f f a a a a a a 
+    . a f f f f a a a a f f f a a a 
+    . . f f f f f a a f f f f f a . 
+    . . . f f f . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.ay = 400
+mySprite.vx = 100
+scene.cameraFollowSprite(mySprite)
